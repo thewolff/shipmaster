01f5cccf3af3e3befc9e4a0008b8c14696945c45
WOLFF, JOSEPH <wolffjoe@amazon.com>
2017-06-01 15:33:30 -0700

    ported and works

--- shippers/guess.js
+++ shippers/guess.js
@@ -5,35 +5,169 @@ const _ = require('lodash');
 const upperCase = require('change-case').upperCase;
 const uniq = require('lodash').uniq;
 
-const _confirmUps = id => {};
+const _checkDigit = (id, multi, mod) => {
+  var checkdigit, index, midx, sum, _i, _ref;
+  midx = 0;
+  sum = 0;
+  for (
+    (index = _i = 0), (_ref = id.length - 2);
+    0 <= _ref ? _i <= _ref : _i >= _ref;
+    index = 0 <= _ref ? ++_i : --_i
+  ) {
+    sum += parseInt(id[index], 10) * multi[midx];
+    midx = midx === multi.length - 1 ? 0 : midx + 1;
+  }
+  if (mod === 11) {
+    checkdigit = sum % 11;
+    if (checkdigit === 10) {
+      checkdigit = 0;
+    }
+  }
+  if (mod === 10) {
+    checkdigit = 0;
+    if (sum % 10 > 0) {
+      checkdigit = 10 - sum % 10;
+    }
+  }
+  return checkdigit === parseInt(id[id.length - 1]);
+};
 
-const _confirmUpsFreight = id => {};
+const _confirmUps = id => {
+  var asciiValue, checkdigit, index, num, sum, _i;
+  sum = 0;
+  for (index = _i = 2; _i <= 16; index = ++_i) {
+    asciiValue = id[index].charCodeAt(0);
+    if (asciiValue >= 48 && asciiValue <= 57) {
+      num = parseInt(id[index], 10);
+    } else {
+      num = (asciiValue - 63) % 10;
+    }
+    if (index % 2 !== 0) {
+      num = num * 2;
+    }
+    sum += num;
+  }
+  checkdigit = sum % 10 > 0 ? 10 - sum % 10 : 0;
+  if (checkdigit === parseInt(id[17], 10)) {
+    return [true, true];
+  }
+  return [false, false];
+};
 
-const _confirmFedex12 = id => {};
+const _confirmUpsFreight = id => {
+  var firstChar, remaining;
+  firstChar = '' + (id.charCodeAt(0) - 63) % 10;
+  remaining = id.slice(1);
+  id = '' + firstChar + remaining;
+  if (_checkDigit(id, [3, 1, 7], 10)) {
+    return [true, true];
+  }
+  return [false, false];
+};
 
-const _confirmFedex15 = id => {};
+const _confirmFedex12 = id => {
+  if (_checkDigit(id, [3, 1, 7], 11)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmFedex20 = id => {};
+const _confirmFedex15 = id => {
+  if (_checkDigit(id, [1, 3], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmUsps20 = id => {};
+const _confirmFedex20 = id => {
+  var alteredid;
+  if (_checkDigit(id, [3, 1, 7], 11)) {
+    return [true, false];
+  } else {
+    alteredid = '92' + id;
+    if (_checkDigit(alteredid, [3, 1], 10)) {
+      return [true, false];
+    }
+  }
+  return [false, false];
+};
 
-const _confirmFedexSmartPost = id => {};
+const _confirmFedexSmartPost = id => {
+  if (_checkDigit('91' + id, [3, 1], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmFedexDoorTag = id => {};
+const _confirmFedexDoorTag = id => {
+  if (_checkDigit(id.match(/^DT(\d{12})$/)[1], [3, 1, 7], 11)) {
+    return [true, true];
+  }
+  return [false, false];
+};
 
-const _confirmFedex9622 = id => {};
+const _confirmFedex9622 = id => {
+  if (_checkDigit(id, [3, 1, 7], 11)) {
+    return [true, false];
+  }
+  if (_checkDigit(id.slice(7), [1, 3], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmUsps22 = id => {};
+const _confirmUsps20 = id => {
+  if (_checkDigit(id, [3, 1], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmUsps26 = id => {};
+const _confirmUsps22 = id => {
+  if (_checkDigit(id, [3, 1], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmUsps420Zip = id => {};
+const _confirmUsps26 = id => {
+  if (_checkDigit(id, [3, 1], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmUsps420ZipPlus4 = id => {};
+const _confirmUsps420Zip = id => {
+  if (_checkDigit(id.match(/^420\d{5}(\d{22})$/)[1], [3, 1], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
-const _confirmCanadaPost16 = id => {};
+const _confirmUsps420ZipPlus4 = id => {
+  if (_checkDigit(id.match(/^420\d{9}(\d{22})$/)[1], [3, 1], 10)) {
+    return [true, false];
+  } else {
+    if (_checkDigit(id.match(/^420\d{5}(\d{26})$/)[1], [3, 1], 10)) {
+      return [true, false];
+    }
+  }
+  return [false, false];
+};
 
-const _confirmA1International = id => {};
+const _confirmCanadaPost16 = id => {
+  if (_checkDigit(id, [3, 1], 10)) {
+    return [true, false];
+  }
+  return [false, false];
+};
+
+const _confirmA1International = id => {
+  if (id.length === 9 || id.length === 13) {
+    return [true, false];
+  }
+  return [false, false];
+};
 
 const CARRIERS = [
   {
@@ -213,12 +347,14 @@ const _preProcess = id => {
 class Guess {
   constructor(id) {
     let pid = _preProcess(id);
+    let carriers = [];
     CARRIERS.every(carrier => {
       let good, stop, _ref;
       if (pid.match(carrier.regex)) {
         if (carrier.confirm != null) {
-          (_ref = carrier.confirm(pid)), (good = _ref[0]), (stop = _ref[1]);
-          console.log('ref', _ref);
+          _ref = carrier.confirm(pid);
+          good = _ref[0];
+          stop = _ref[1];
           if (good) {
             carriers.push(carrier.name);
           }
